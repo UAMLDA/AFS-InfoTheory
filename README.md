d79ac327b090f11646a114d9c20b1f06f8ec437c
Roman Golota <rpgolota@gmail.com>
2020-12-11 14:04:57 -0700

    changed clean attacks and reran poisoning currently 72%

--- README.md
+++ README.md
@@ -4,10 +4,8 @@
 
 To run `scripts\poison_clean_data.py` you need to `pip install -r requirements.txt`, and also need to have [poisoning](https://github.com/rpgolota/poisoning/) installed from github, as it is not on PyPi.
 
-Current attack creation progress is about 25%, but that is misleading since it is going from smallest to largest in terms of sample size.
-
 - To create attacks, run the script `scripts\poison_clean_data.py`. It should resume where it left off.
 - Currently it is running from the smallest to the largest datasets located in `data\filenames_by_samples.txt`.
 - Attack file naming convention is:
-  - (filename no path)\_attack\_[(projection)].(extension)
+  - (filename no path)\_[(type)][(projection)].(extension)
 - All output files go to `data\attacks`. All original files are in `data\clean`.
--- scripts/poison_clean_data.py
+++ scripts/poison_clean_data.py
@@ -7,13 +7,16 @@ import os
 
 from rich.progress import Progress
 
-DATA_PATH = '../data'
+DATA_PATH = os.path.join(os.path.dirname(os.path.abspath(__file__)), '../data')
 CLEAN_PATH = os.path.join(DATA_PATH, 'clean')
 ATTACKS_PATH = os.path.join(DATA_PATH, 'attacks')
 FILENAMES_PATH = 'filenames_by_samples.txt'
 
 POISON_PERCENT = 0.2
 POISON_PROJECTIONS = [0.5, 1, 1.5, 2, 2.5, 5]
+POISON_TYPES = [poisoning.xiao2018]
+
+types_name = {poisoning.xiao2018: 'xiao', poisoning.frederickson2018: 'fred'}
 
 VERBOSE = True
 
@@ -24,9 +27,9 @@ def write_attacks(filename, poisoned, labels):
     final.to_csv(os.path.join(ATTACKS_PATH, filename), sep=',', header=None, index=None)
 
 # for getting the filename of the attack file
-def create_filename(filename, projection):
+def create_filename(filename, projection, tp):
     file, extension = os.path.splitext(os.path.basename(filename))
-    return file + '_attack_[' + str(projection) + ']' + extension
+    return f"{file}_[{types_name[tp]}][{projection}]{extension}"
 
 # function to poison a dataset, gets passed a function to update progress bar
 def poison_clean(filename, progress_update):
@@ -35,21 +38,26 @@ def poison_clean(filename, progress_update):
     X = dataset.iloc[:,:-1].values
     Y = dataset.iloc[:,-1].values
     
-    for projection in POISON_PROJECTIONS:
-        new_filename = create_filename(filename, projection)
+    for tp in POISON_TYPES:
         
-        if VERBOSE:
-            print(f'    + Projection: {projection}')
+        for projection in POISON_PROJECTIONS:
         
-        # if file exists already, skip
-        if os.path.exists(os.path.join(ATTACKS_PATH, new_filename)):
-            progress_update()
-            continue
+            new_filename = create_filename(filename, projection, tp)
+            
+            if VERBOSE:
+                print(f'    + Type: {types_name[tp]} :: Projection: {projection}')
         
-        model = poisoning.xiao2018()
-        poisoned, labels = model.autorun(X, Y, POISON_PERCENT, projection)
-        write_attacks(new_filename, poisoned, labels)
-        progress_update()
+            # if file exists already, skip
+            if os.path.exists(os.path.join(ATTACKS_PATH, new_filename)):
+                progress_update(2)
+                continue
+            
+            model = tp()
+            poisoned, labels = model.autorun(X, Y, POISON_PERCENT, projection)
+            write_attacks(new_filename, poisoned, labels)
+            progress_update(2)
+            
+        progress_update(1)
 
 # Main entry point for script
 def main():
@@ -57,16 +65,19 @@ def main():
         files = [file.strip() for file in f.readlines()]
         
     with Progress() as progress:
-        t1 = progress.add_task("[bold red]Running Files...", total=len(files))
-        t2 = progress.add_task("[bold blue]Running Models...", total=len(POISON_PROJECTIONS))
-        def update():
-            progress.advance(t2)
+        t1 = progress.add_task(f"[bold red]Running Files ({len(files)})...", total=len(files))
+        t2 = progress.add_task(f"[bold blue]Running Models ({len(POISON_TYPES)})...", total=len(POISON_TYPES))
+        t3 = progress.add_task(f"[bold blue]Running Projections ({len(POISON_PROJECTIONS)})...", total=len(POISON_PROJECTIONS))
+        proc = [t1, t2, t3]
+        def update(i):
+            progress.advance(proc[i])
         for file in files:
             if VERBOSE:
                 print(f'File: {file}')
             poison_clean(file, update)
             progress.advance(t1)
             progress.reset(t2)
+            progress.reset(t3)
 
 if __name__ == "__main__":
     main()
