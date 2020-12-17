8bd3fcc2458b7d76b81958f00ffcd135f00ea834
Gregory Ditzler <gregory.ditzler@gmail.com>
2020-12-17 00:44:37 -0500

    added outline to the file

--- runexp.py
+++ runexp.py
@@ -23,27 +23,54 @@
 import numpy as np 
 import pandas as pd 
 import skfeature as skf
-
 import argparse
-
 from utils import kuncheva, jaccard
+from skfeature.function.information_theoretical_based import JMI, MIM, MRMR, MIFS
 
 # setup program constants 
 data_folder = 'data/'
+POI_RNG = [.01, .025, .05, .075, .1, .125, .15, .175, .2]
+NPR = len(POI_RNG)
+SEL_PERCENT = .1
+NALG = 4
+FEAT_IDX = 0
 
-def main(args):
+def main():
+    #data, box, cv = args.data, args.box, args.cv
+    data, box, cv = 'blood', '1', 5
 
     # load normal and adversarial data 
-    df_normal = pd.read_csv('data/clean/' + args.data + '.csv')
-    df_adversarial = pd.read_csv('data/attacks/' + args.data + '_[xiao][' + str(.5) + '].csv') 
+    path_adversarial_data = 'data/attacks/' + data + '_[xiao][' + box + '].csv'
+    df_normal = pd.read_csv('data/clean/' + data + '.csv', header=None).values
+    df_adversarial = pd.read_csv(path_adversarial_data, header=None).values
+    Xn, yn = df_normal[:,:-1], df_normal[:,-1]
+    Xa, ya = df_adversarial[:,:-1], df_adversarial[:,-1]
 
-    # load adversarial data
-    n = 10000
-    n_train = 1000
+    # change the labels 
+    ya[ya==-1], yn[yn==-1] = 0, 0
 
-    for k in range(args.cv): 
+    # set up the data numers 
+    p0, p1 = 1./cv, (1. - 1./cv)
+    N = len(Xn)
+    Ntr, Nte = int(p1*N), int(p0*N)
+    n_selected_features = int(Xn.shape[1]*SEL_PERCENT)+2
+    Npoi = np.array(POI_RNG)*Ntr
+
+    idx_jaccard, idx_kuncheva = np.zeros((NPR, NALG)), np.zeros((NPR, NALG))    
+
+    for k in range(cv): 
         # shuffle up the data for the experiment 
-        k
+        i = np.random.permutation(N)
+        Xtrk, ytrk, Xtek, ytek = Xn[i][:Ntr], yn[i][:Ntr], Xn[i][Nte:], yn[i][Nte:]
+
+        # run feature selection on the baseline dataset without an adversarial data 
+        sf_base_jmi, sf_base_mim, sf_base_mrmr, sf_base_mifs = \
+            JMI.jmi(Xtrk, ytrk, n_selected_features=n_selected_features), \
+            MIM.mim(Xtrk, ytrk, n_selected_features=n_selected_features), \
+            MRMR.mrmr(Xtrk, ytrk, n_selected_features=n_selected_features), \
+            MIFS.mifs(Xtrk, ytrk, n_selected_features=n_selected_features)
+
+        print(jaccard(sf_base_jmi[FEAT_IDX], sf_base_mim[FEAT_IDX]))
     # average out stability
 
     # write the results to a file
@@ -51,6 +78,7 @@ def main(args):
 
 if __name__ == '__main__': 
     # set up the parser
+    '''
     parser = argparse.ArgumentParser(description='Run the experiments for the IJCNN 2021 paper.', 
                                      prog='runexp', 
                                      usage='%(prog)s [options]',) 
@@ -69,8 +97,8 @@ if __name__ == '__main__':
                         help='percentage of poison data to add [float]')
     parser.add_argument('-b', 
                         '--box', 
-                        type=float, 
-                        help='bounding box [float]. note the file must be in the data folder.')
+                        type=str, 
+                        help='bounding box [str]. note the file must be in the data folder.')
     parser.add_argument('-o', 
                         '--output', 
                         type=str, 
@@ -79,3 +107,5 @@ if __name__ == '__main__':
     args = parser.parse_args()
     # run
     main(args)
+    '''
+    main()
