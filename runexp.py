608650a365513f3622d3b26eddce61db727d39eb
Gregory Ditzler <gregory.ditzler@gmail.com>
2020-12-17 21:48:38 -0500

    added comments

--- runexp.py
+++ runexp.py
@@ -110,35 +110,57 @@ def experiment(data, box, cv, output):
     # change the labels from +/-1 to [0,1]
     ya[ya==-1], yn[yn==-1] = 0, 0
 
-    # set up the data numers 
+    # calculate the rattios of data that would be used for training and hold out  
     p0, p1 = 1./cv, (1. - 1./cv)
     N = len(Xn)
+    # calculate the total number of training and testing samples and set the numbfer of 
+    # features that are going to be selected 
     Ntr, Nte = int(p1*N), int(p0*N)
-    n_selected_features = int(Xn.shape[1]*SEL_PERCENT)+2
+    n_selected_features = int(Xn.shape[1]*SEL_PERCENT)+1
 
+    # zero the results out 
     err_jaccard, err_kuncheva = np.zeros((NPR, NALG)), np.zeros((NPR, NALG))    
 
+    # run `cv` randomized experiments. note this is not performing cross-validation, rather
+    # we are going to use randomized splits of the data.  
     for _ in range(cv): 
-        # shuffle up the data for the experiment 
+        # shuffle up the data for the experiment then split the data into a training and 
+        # testing dataset
         i = np.random.permutation(N)
         Xtrk, ytrk, Xtek, ytek = Xn[i][:Ntr], yn[i][:Ntr], Xn[i][Nte:], yn[i][Nte:]
 
-        # run feature selection on the baseline dataset without an adversarial data 
+        # run feature selection on the baseline dataset without an adversarial data. this 
+        # will serve as the baseline. use a parallel assignment to speed things up. 
         sf_base_jmi, sf_base_mim, sf_base_mrmr, sf_base_mifs = \
             JMI.jmi(Xtrk, ytrk, n_selected_features=n_selected_features)[FEAT_IDX], \
             MIM.mim(Xtrk, ytrk, n_selected_features=n_selected_features)[FEAT_IDX], \
             MRMR.mrmr(Xtrk, ytrk, n_selected_features=n_selected_features)[FEAT_IDX], \
             MIFS.mifs(Xtrk, ytrk, n_selected_features=n_selected_features)[FEAT_IDX]
 
+        # loop over the number of poisoning ratios that we need to evaluate
         for n in range(NPR): 
+
+            # calucate the number of poisoned data that we are going to need to make sure 
+            # that the poisoning ratio is correct in the training data. e.g., if you have 
+            # N=100 samples and you want to poison by 20% then the 20% needs to be from 
+            # the training size. hence it is not 20. 
             Np = int(len(ytrk)*POI_RNG[n]+1)
             if Np >= len(ya): 
+                # shouldn't happen but catch the case where we are requesting more poison
+                # data samples than are available. NEED TO BE CAREFUL WHEN WE ARE CREATING 
+                # THE ADVERSARIAL DATA
                 ValueError('Number of poison data requested is larger than the available data.')
+
+            # find the number of normal samples (i.e., not poisoned) samples in the 
+            # training data. then create the randomized data set that has Nn normal data
+            # samples and Np adversarial samples in the training data
             Nn = len(ytrk) - Np
             idx_normal, idx_adversarial = np.random.permutation(len(ytrk))[:Nn], \
                                           np.random.permutation(len(ya))[:Np]
             Xtrk_poisoned, ytrk_poisoned = np.concatenate((Xtrk[idx_normal], Xa[idx_adversarial])), \
                                            np.concatenate((ytrk[idx_normal], ya[idx_adversarial]))
+
+            # run feature selection with the training data that has adversarial samples
             sf_adv_jmi, sf_adv_mim, sf_adv_mrmr, sf_adv_mifs = \
                 JMI.jmi(Xtrk_poisoned, ytrk_poisoned, n_selected_features=n_selected_features)[FEAT_IDX], \
                 MIM.mim(Xtrk_poisoned, ytrk_poisoned, n_selected_features=n_selected_features)[FEAT_IDX], \
@@ -157,6 +179,7 @@ def experiment(data, box, cv, output):
             err_kuncheva[n, 2] += kuncheva(sf_adv_mrmr, sf_base_mrmr, Xtrk.shape[1])
             err_kuncheva[n, 3] += kuncheva(sf_adv_jmi, sf_base_jmi, Xtrk.shape[1])
 
+    # scale the kuncheva and jaccard statistics by 1.0/cv then write the output file
     err_jaccard,  err_kuncheva = err_jaccard/cv, err_kuncheva/cv
     np.savez(output, err_jaccard=err_jaccard, err_kuncheva=err_kuncheva)
 
